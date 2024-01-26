f92c5b37d57c6990f31b9e64078231852fcce4c9
wyx2685 <wyx2685@gmail.com>
2024-01-27 01:55:43 +0900

    尝试优化代码

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	_ "net/http/pprof"
-
 	"github.com/InazumaV/V2bX/cmd"
 )
 
 func main() {
-	//内存泄漏排查
-	//	go func() {
-	//		http.ListenAndServe("127.0.0.1:6060", nil)
-	//	}()
 	cmd.Run()
 }
